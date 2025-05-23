f6270db55b64003a37ab3bd3baba79ce5121efd1
HnrqHolanda <henriqholanda1@gmail.com>
2025-05-23 01:02:38 -0300

    Atualização da  Head nas páginas

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,27 +1,36 @@
-import Header from "@/components/Header";
-import BackgroundBlobs from "@/components/BackgroundBlobs";
-import Content1 from "@/components/Content1";
-import Footer from "@/components/Footer";
+import Head from 'next/head';
+import Header from '@/components/Header';
+import BackgroundBlobs from '@/components/BackgroundBlobs';
+import Content1 from '@/components/Content1';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen text-white overflow-hidden">
-      <Header />
-      <BackgroundBlobs />
+    <>
+      <Head>
+        <title>Portfolio - HH</title>
+        <link rel="icon" href="/icon.png" />
+      </Head>
 
-      {/* Conteúdo + Footer juntos em 100vh */}
-      <main className="relative z-10 flex flex-col justify-between min-h-screen">
-        {/* Conteúdo centralizado */}
-        <section className="flex-1 flex items-center justify-center px-6">
-          <Content1 />
-        </section>
+      <div className="relative min-h-screen text-white overflow-hidden">
+        <Header />
+        <BackgroundBlobs />
 
-        {/* Footer fixo ao final */}
-        <Footer />
-      </main>
-    </div>
+        {/* Conteúdo + Footer juntos em 100vh */}
+        <main className="relative z-10 flex flex-col justify-between min-h-screen">
+          {/* Conteúdo centralizado */}
+          <section className="flex-1 flex items-center justify-center px-6">
+            <Content1 />
+          </section>
+
+          {/* Footer fixo ao final */}
+          <Footer />
+        </main>
+      </div>
+    </>
   );
 }
+
 
 
 
--- src/pages/projetos.jsx
+++ src/pages/projetos.jsx
@@ -2,20 +2,29 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import BackgroundBlobs from '@/components/BackgroundBlobs';
 import ProjetosSection from '@/components/ProjetosSection';
+import Head from 'next/head';
 
 export default function Projetos() {
   return (
-    <div className="text-white flex flex-col min-h-screen">
-      <Header />
-      <BackgroundBlobs />
+    <>
+      <Head>
+        <title>Portfolio - HH</title>
+        <link rel="icon" href="/icon.png" />
+      </Head>
 
-      <main className="flex flex-col relative z-20 flex-1">
-        <section className="flex-1">
-          <ProjetosSection />
-        </section>
-        <Footer />
-      </main>
-    </div>
+      <div className="text-white flex flex-col min-h-screen">
+        <Header />
+        <BackgroundBlobs />
+
+        <main className="flex flex-col relative z-20 flex-1">
+          <section className="flex-1">
+            <ProjetosSection />
+          </section>
+          <Footer />
+        </main>
+      </div>
+    </>
   );
 }
+
 
